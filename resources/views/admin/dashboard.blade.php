34e7cc07c02cb7495493467e275c3a95b0f45865
Seaw Boon <seawboon.yeap@gmail.com>
2021-10-01 12:11:18 +0800

    dashboard template

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.app')
+
+@section('content')
+  @include('admin.templates.widgets')
+  @include('admin.templates.chart')
+  @include('admin.templates.widgets-2')
+  @include('admin.templates.table')
+  @include('admin.templates.widgets-3')
+@endsection
---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Analytics Dashboard - This is an example dashboard created using build-in elements and components.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+    <meta name="description" content="This is an example dashboard created using build-in elements and components.">
+    <meta name="msapplication-tap-highlight" content="no">
+    <link href="{{ asset('fonts.css') }}" rel="stylesheet"></head>
+    <link href="{{ asset('ui.css?v2') }}" rel="stylesheet"></head>
+<body>
+    <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+        @include('admin.layouts.header')
+        @include('admin.layouts.ui-theme-settings')
+        <div class="app-main">
+                @include('admin.layouts.sidebar')
+                <div class="app-main__outer">
+                    <div class="app-main__inner">
+                          @include('admin.layouts.pages.title')
+                          @yield('content')
+
+                    </div>
+                    @include('admin.layouts.footer')
+                        </div>
+                <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        </div>
+    </div>
+<script type="text/javascript" src="{{ asset('assets/scripts/main.js') }}"></script></body>
+</html>
